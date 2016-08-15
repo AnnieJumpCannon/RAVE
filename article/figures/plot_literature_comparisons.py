3fe56a8afbbf7be8062ad4a4abdbe33dd2718973
Andy Casey <andycasey@gmail.com>
2016-08-15 09:42:19 +0100

    Samples (other than surveys like GES) to compare against

---
+++ article/figures/plot_literature_comparisons.py
@@ -0,0 +1,8 @@
+
+"""
+Ruchti et al (2011); 229 spectra
+CFLIB (Pastel database; 224+169 spectra)
+Fulbright in prep (163 spectra)
+M67 (Pancino et al 2010; 16 spectra)
+IC4651 (Pasquini et al 2004; 6 spectra)
+"""
