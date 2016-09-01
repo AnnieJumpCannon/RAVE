c12a09a99e9f8beb205f1b1b38648a1872329086
Andy Casey <andycasey@gmail.com>
2016-09-01 19:22:16 +0100

    Add gold standard comparisons for v0.7 -- clearly something wrong with model join because these discrepancies don't exist in some prev. models or single m-s model

--- article/figures/plot_gold_standard_comparisons.py
+++ article/figures/plot_gold_standard_comparisons.py
@@ -6,6 +6,8 @@ Reddy et al. (2003, 2006), and Valenti & Fischer (2005).
 
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 try:
     bensby_2014, reddy_2003, reddy_2006, valenti_2005
@@ -19,9 +21,28 @@ except NameError: # Do you know who I am? That's Jeff Vader!
     from astropy.table import join
 
     rave_cannon_dr1 = get_cannon_dr1()
+
+    """
+    #rave_cannon_dr1["Name"] = rave_cannon_dr1["Name_1"]
+    if "EPIC_TEFF" in rave_cannon_dr1.dtype.names:
+
+        rave_cannon_dr1["TEFF"] = rave_cannon_dr1["EPIC_TEFF"]
+        rave_cannon_dr1["LOGG"] = rave_cannon_dr1["EPIC_LOGG"]
+        rave_cannon_dr1["FE_H"] = rave_cannon_dr1["EPIC_FEH"]
+
+    if "SNRK" not in rave_cannon_dr1.dtype.names:
+        from rave_io import get_rave_kordopatis_dr4
+        rave_cannon_dr1 = join(rave_cannon_dr1, get_rave_kordopatis_dr4(), keys=("Name", ))
     
-    OK = (rave_cannon_dr1["SNRK"] > 10) * (rave_cannon_dr1["R_CHI_SQ"] < 3) * (rave_cannon_dr1["R"] > 25)
+    if "R_CHI_SQ" not in rave_cannon_dr1.dtype.names:
+        rave_cannon_dr1["R_CHI_SQ"] = rave_cannon_dr1["r_chi_sq"]
+    
+    OK = (rave_cannon_dr1["SNRK"] > 10) * (rave_cannon_dr1["R"] > 25) * (rave_cannon_dr1["R_CHI_SQ"] < 3)
     rave_cannon_dr1 = rave_cannon_dr1[OK].filled()
+    """
+
+    ok = (rave_cannon_dr1["snr"] > 10) * (rave_cannon_dr1["r_chi_sq"] < 3)
+    rave_cannon_dr1 = rave_cannon_dr1[ok].filled()
 
     bensby_2014 = join(rave_cannon_dr1, get_literature_bensby(), keys=("Name", ))
     reddy_2003 = join(rave_cannon_dr1, get_literature_reddy_2003(), keys=("Name", ))
@@ -45,10 +66,10 @@ def get_data(table, cannon_label, translated_label):
 def bensby_2014_data(cannon_label):
 
     label_name = {
-        "TEFF": "Teff",
-        "LOGG": "logg",
+        "TEFF": "TeffH",
+        "LOGG": "loggH",
         "FE_H": "Fe_H"
-    }[cannon_label]
+    }.get(cannon_label, cannon_label)
 
     return get_data(bensby_2014, cannon_label, label_name)
 
@@ -59,7 +80,7 @@ def reddy_2003_data(cannon_label):
         "TEFF": "Teff",
         "LOGG": "logg",
         "FE_H": "__Fe_H__2"
-    }[cannon_label]
+    }.get(cannon_label, cannon_label)
 
     return get_data(reddy_2003, cannon_label, label_name)
 
@@ -70,7 +91,7 @@ def reddy_2006_data(cannon_label):
         "TEFF": "Teff",
         "LOGG": "logg",
         "FE_H": "__Fe_H__2"
-    }[cannon_label]
+    }.get(cannon_label, cannon_label)
 
     return get_data(reddy_2006, cannon_label, label_name)
 
@@ -81,46 +102,151 @@ def valenti_2005_data(cannon_label):
         "TEFF": "Teff",
         "LOGG": "log_g_",
         "FE_H": "__Fe_H__2"
-    }[cannon_label]
+    }.get(cannon_label, cannon_label)
 
     return get_data(valenti_2005, cannon_label, label_name)
 
 
 
-comparisons = {
-    bensby_2014_data: dict(facecolor="b", marker="s"),
-    reddy_2006_data: dict(facecolor="r", marker="o"),
-    reddy_2003_data: dict(facecolor="w", edgecolor="k", linewidth=2, marker="o"),
-    valenti_2005_data: dict(facecolor="g", marker="^")
-}
+
+from collections import OrderedDict
+comparisons = OrderedDict([
+    [bensby_2014_data, dict(s=75, marker="s", )], #facecolor="#EEEFF7", marker="s", zorder=0)],
+    [reddy_2003_data, dict(s=75, marker="o", )], #facecolor="#31353D", marker="o", )],
+    [reddy_2006_data, dict(s=100, marker="*", )], # facecolor="#92CDCF", marker="o", )],
+    [valenti_2005_data, dict(s=75, marker="^", )], #facecolor="#445878", marker="^", )],
+])
+comparison_labels = (
+    r"${\rm Bensby}$ ${\rm et}$ ${\rm al.}$ $(2014)$",
+    r"${\rm Reddy}$ ${\rm et}$ ${\rm al.}$ $(2003)$", 
+    r"${\rm Reddy}$ ${\rm et}$ ${\rm al.}$ $(2006)$", 
+    r"${\rm Valenti}$ ${\rm \&}$ ${\rm Fischer}$ $(2005)$") 
 
 label_names = ("TEFF", "LOGG", "FE_H")
 N = len(label_names)
 
-fig, axes = plt.subplots(1, N)
-
-for ax, label_name in zip(axes, label_names):
-
-
-    for comparison, kwds in comparisons.items():
-        x, y = comparison(label_name)
-
-        _, c = comparison("FE_H")
-
-        scat = ax.scatter(x, y, c=c, vmin=-2.5, vmax=0.5, **kwds)
-
-        comp = c > -0.7
-        diff = y[comp] - x[comp]
-        print(comparison, label_name, np.nanmean(diff), np.nanstd(diff))
-
-    limits = np.array([ax.get_xlim(), ax.get_ylim()])
-    limits = [limits.min(), limits.max()]
+def latexify(label_name, axis):
     
-    ax.plot(limits, limits, c="#666666", zorder=-1, linestyle=":")
-    ax.set_xlim(limits)
-    ax.set_ylim(limits)
+    latex = {
+        "TEFF": r"$T_{\rm eff}$",
+        "LOGG": r"$\log{g}$",
+        "FE_H": r"$[{\rm Fe/H}]$"
+    }[label_name]
+
+    suffix = r"$({\rm Literature})$" if axis == "x" else r"$({\rm unRAVE})$"
+
+    return " ".join([latex, suffix])
 
 
-    ax.set_xlabel(label_name)
 
-plt.colorbar(scat)
+
+
+
+#fig, axes = plt.subplots(1, N)
+from matplotlib import gridspec
+
+fig = plt.figure(figsize=(14.15, 5.4))
+gs = gridspec.GridSpec(2, N, width_ratios=[1] * N, height_ratios=[1, 3])
+
+axes = [plt.subplot(gs[i]) for i in range(2 * N)]
+
+handles = []
+
+ylims = {
+    "TEFF": 1200,
+    "LOGG": 2,
+    "FE_H": 1
+}
+limits = {
+    "TEFF": (4000, 7000),
+    "LOGG": (2.5, 5.5),
+    "FE_H": (-2.5, 0.5)
+}
+for ax, label_name in zip(axes, list(label_names) + list(label_names)):
+
+    allx = []
+    ally = []
+    diffs = []
+    for comparison, kwds in comparisons.items():
+        x, y = comparison(label_name) 
+        c0, c1 = comparison("snr")
+        allx.extend(x)
+        ally.extend(y)
+        if ax.is_first_row():
+            _ = ax.scatter(x, y - x, c=c0, cmap="plasma", **kwds)
+            diffs.extend(y-x)
+
+            if ax == axes[0]:
+                handles.append(ax.scatter([-10000], [-10000], facecolor="k", **kwds))
+
+        else:
+            scat = ax.scatter(x, y, c=c0, cmap="plasma", **kwds)
+    print(label_name, np.nanmedian(diffs), np.nanstd(diffs))
+
+    if ax.is_first_row():
+        ax.set_xticklabels([])
+
+        ylim = ylims[label_name]
+        ax.set_ylim(-ylim, +ylim)
+        ax.axhline(0, c="#666666", zorder=-1, linestyle=":")
+
+        ax.yaxis.set_major_locator(MaxNLocator(5))
+
+    else:
+
+        # calculate line coeffs
+
+        x = np.array(allx)
+        y = np.array(ally)
+        yerr = np.ones(x.size)
+ 
+        A = np.vstack((np.ones_like(x), x)).T
+        C = np.diag(yerr * yerr)
+        cov = np.linalg.inv(np.dot(A.T, np.linalg.solve(C, A)))
+        b, m = np.dot(cov, np.dot(A.T, np.linalg.solve(C, y)))
+
+
+        A = np.vstack((np.ones_like(y), y)).T
+        C = np.diag(yerr*yerr)
+        cov = np.linalg.inv(np.dot(A.T, np.linalg.solve(C, A)))
+        b2, m2 = np.dot(cov, np.dot(A.T, np.linalg.solve(C, x)))
+
+        print(label_name, m2, b2)
+        
+        
+        
+        #ax.plot(limits, m*limits + b, c='r', zorder=0)
+        limit = limits[label_name]
+        ax.plot(limit, limit, c="#666666", zorder=-1, linestyle=":")
+        ax.set_xlim(limit)
+        ax.set_ylim(limit)
+
+        axes[axes.index(ax) - N].set_xlim(limit)
+
+        ax.set_xlabel(latexify(label_name, "x"))
+        ax.set_ylabel(latexify(label_name, "y"))
+        
+
+        ax.xaxis.set_major_locator(MaxNLocator(5))
+        ax.yaxis.set_major_locator(MaxNLocator(5))
+
+
+fig.legend(handles, comparison_labels, loc="upper center", 
+    frameon=False, ncol=len(comparison_labels), numpoints=1)
+
+#divider = make_axes_locatable(axes[-1])
+#cax = divider.append_axes("right", size="5%", pad=0.05)
+
+fig.tight_layout()
+
+
+cbar = plt.colorbar(scat, cax=fig.add_axes([0.93,fig.subplotpars.bottom,0.02,0.9 - fig.subplotpars.bottom]))
+cbar.set_label(r"${\rm RAVE}$ $S/N$ $[{\rm pixel}^{-1}]$")
+
+fig.subplots_adjust(top=0.90, right=0.91)
+
+fig.savefig("gold-standard-comparison.png")
+fig.savefig("gold-standard-comparison.pdf", dpi=300)
+
+
+
---
+++ article/figures/plot_gold_standard_hrd.py
@@ -0,0 +1,285 @@
+
+"""
+Compare our results to the gold standard studies of Bensby et al. (2014), 
+Reddy et al. (2003, 2006), and Valenti & Fischer (2005).
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+try:
+    bensby_2014, reddy_2003, reddy_2006, valenti_2005
+
+except NameError: # Do you know who I am? That's Jeff Vader!
+
+    from rave_io import \
+        (get_cannon_dr1, get_literature_bensby, get_literature_reddy_2003,
+            get_literature_reddy_2006, get_literature_valenti_2005)
+
+    from astropy.table import join
+
+    rave_cannon_dr1 = get_cannon_dr1()
+
+    """
+    #rave_cannon_dr1["Name"] = rave_cannon_dr1["Name_1"]
+    if "EPIC_TEFF" in rave_cannon_dr1.dtype.names:
+
+        rave_cannon_dr1["TEFF"] = rave_cannon_dr1["EPIC_TEFF"]
+        rave_cannon_dr1["LOGG"] = rave_cannon_dr1["EPIC_LOGG"]
+        rave_cannon_dr1["FE_H"] = rave_cannon_dr1["EPIC_FEH"]
+
+    if "SNRK" not in rave_cannon_dr1.dtype.names:
+        from rave_io import get_rave_kordopatis_dr4
+        rave_cannon_dr1 = join(rave_cannon_dr1, get_rave_kordopatis_dr4(), keys=("Name", ))
+    
+    if "R_CHI_SQ" not in rave_cannon_dr1.dtype.names:
+        rave_cannon_dr1["R_CHI_SQ"] = rave_cannon_dr1["r_chi_sq"]
+    
+    OK = (rave_cannon_dr1["SNRK"] > 10) * (rave_cannon_dr1["R"] > 25) * (rave_cannon_dr1["R_CHI_SQ"] < 3)
+    rave_cannon_dr1 = rave_cannon_dr1[OK].filled()
+    """
+
+    ok = (rave_cannon_dr1["snr"] > 50) * (rave_cannon_dr1["r_chi_sq"] < 3)
+    rave_cannon_dr1 = rave_cannon_dr1[ok].filled()
+
+    bensby_2014 = join(rave_cannon_dr1, get_literature_bensby(), keys=("Name", ))
+    reddy_2003 = join(rave_cannon_dr1, get_literature_reddy_2003(), keys=("Name", ))
+    reddy_2006 = join(rave_cannon_dr1, get_literature_reddy_2006(), keys=("Name", ))
+    valenti_2005 = join(rave_cannon_dr1, get_literature_valenti_2005(), keys=("Name", ))
+
+else:
+    print("Using pre-loaded data")
+
+
+# Create convenience functions 
+
+def get_data(table, cannon_label, translated_label):
+    x = table[translated_label]
+    #xerr #TODO
+    y = table[cannon_label]
+    # yerr #TODO
+    return (x, y)
+
+
+def bensby_2014_data(cannon_label):
+
+    label_name = {
+        "TEFF": "TeffH",
+        "LOGG": "loggH",
+        "FE_H": "Fe_H"
+    }.get(cannon_label, cannon_label)
+
+    return get_data(bensby_2014, cannon_label, label_name)
+
+
+def reddy_2003_data(cannon_label):
+
+    label_name = {
+        "TEFF": "Teff",
+        "LOGG": "logg",
+        "FE_H": "__Fe_H__2"
+    }.get(cannon_label, cannon_label)
+
+    return get_data(reddy_2003, cannon_label, label_name)
+
+
+def reddy_2006_data(cannon_label):
+
+    label_name = {
+        "TEFF": "Teff",
+        "LOGG": "logg",
+        "FE_H": "__Fe_H__2"
+    }.get(cannon_label, cannon_label)
+
+    return get_data(reddy_2006, cannon_label, label_name)
+
+
+def valenti_2005_data(cannon_label):
+
+    label_name = {
+        "TEFF": "Teff",
+        "LOGG": "log_g_",
+        "FE_H": "__Fe_H__2"
+    }.get(cannon_label, cannon_label)
+
+    return get_data(valenti_2005, cannon_label, label_name)
+
+
+
+
+
+
+from collections import OrderedDict
+comparisons = OrderedDict([
+    [bensby_2014_data, dict(s=50, marker="s", )], #facecolor="#EEEFF7", marker="s", zorder=0)],
+    [reddy_2003_data, dict(s=50, marker="o", )], #facecolor="#31353D", marker="o", )],
+    [reddy_2006_data, dict(s=75, marker="*", )], # facecolor="#92CDCF", marker="o", )],
+    [valenti_2005_data, dict(s=50, marker="^", )], #facecolor="#445878", marker="^", )],
+])
+comparison_labels = (
+    r"${\rm Bensby}$ ${\rm et}$ ${\rm al.}$ $(2014)$",
+    r"${\rm Reddy}$ ${\rm et}$ ${\rm al.}$ $(2003)$", 
+    r"${\rm Reddy}$ ${\rm et}$ ${\rm al.}$ $(2006)$", 
+    r"${\rm Valenti}$ ${\rm \&}$ ${\rm Fischer}$ $(2005)$") 
+
+label_names = ("TEFF", "LOGG", "FE_H")
+N = len(label_names)
+
+def latexify(label_name, axis):
+    
+    latex = {
+        "TEFF": r"$T_{\rm eff}$",
+        "LOGG": r"$\log{g}$",
+        "FE_H": r"$[{\rm Fe/H}]$"
+    }[label_name]
+
+    suffix = r"$({\rm Literature})$" if axis == "x" else r"$({\rm unRAVE})$"
+
+    return " ".join([latex, suffix])
+
+
+
+
+
+
+
+# Make another 4-panel plot showing the HRD for:
+# Bensby, Reddy, Valenti & Fishcer, unRAVE.
+
+
+comparison_labels = (
+    r"${\rm Bensby}$ ${\rm et}$ ${\rm al.}$ $(2014)$",
+    r"${\rm Reddy}$ ${\rm et}$ ${\rm al.}$ $(2003,2006)$", 
+    r"${\rm Valenti}$ ${\rm \&}$ ${\rm Fischer}$ $(2005)$",
+#    r"${\rm Kordopatis}$ ${\rm et}$ ${\rm al.}$ $(2013{\rm ;}$ ${\rm RAVE}$ ${\rm DR4})$",
+    r"${\rm unRAVE}$"
+    )
+M = len(comparison_labels)
+
+link = False
+
+factor = 3.5
+lbdim = 0.2 * factor
+trdim = 0.1 * factor
+whspace = 0.05
+yspace = factor
+xspace = factor * M + factor * (M - 1) * whspace + lbdim * (M - 1)
+xdim = lbdim + xspace + trdim
+ydim = lbdim + yspace + trdim
+
+fig, axes = plt.subplots(1, M, figsize=(xdim, ydim))
+fig.subplots_adjust(
+    left=lbdim/xdim, bottom=lbdim/ydim, right=(xspace + lbdim)/xdim,
+    top=(yspace + lbdim)/ydim, wspace=whspace, hspace=whspace)
+
+kwds = dict(s=50, cmap="plasma", vmin=-1.5, vmax=0.5)
+
+x, ux = bensby_2014_data("TEFF")
+y, uy = bensby_2014_data("LOGG")
+c0, uc = bensby_2014_data("FE_H")
+axes[0].scatter(x, y, c=c0, **kwds)
+
+if link:
+    axes[0].scatter(ux, uy, c=uc, alpha=0.5, marker="^", zorder=-1, **kwds)
+    for xi, uxi, uy, uyi in zip(x, ux, y, uy):
+        axes[0].plot([xi, uxi], [uy, uyi], c="#666666", zorder=-2, linewidth=0.5,
+            alpha=0.5)
+
+
+x, ux = reddy_2003_data("TEFF")
+y, uy = reddy_2003_data("LOGG")
+c0, uc = reddy_2003_data("FE_H")
+axes[1].scatter(x, y, c=c0, **kwds)
+
+if link:
+    axes[1].scatter(ux, uy, c=uc, alpha=0.5, marker="^", zorder=-1, **kwds)
+    for xi, uxi, uy, uyi in zip(x, ux, y, uy):
+        axes[1].plot([xi, uxi], [uy, uyi], c="#666666", zorder=-2, linewidth=0.5,
+            alpha=0.5)
+
+
+
+x, ux = reddy_2006_data("TEFF")
+y, uy = reddy_2006_data("LOGG")
+c0, uc = reddy_2006_data("FE_H")
+axes[1].scatter(x, y, c=c0, **kwds)
+
+if link:
+    axes[1].scatter(ux, uy, c=uc, alpha=0.5, marker="^", zorder=-1, **kwds)
+    for xi, uxi, uy, uyi in zip(x, ux, y, uy):
+        axes[1].plot([xi, uxi], [uy, uyi], c="#666666", zorder=-2, linewidth=0.5,
+            alpha=0.5)
+
+
+
+x, ux = valenti_2005_data("TEFF")
+y, uy = valenti_2005_data("LOGG")
+c0, uc = valenti_2005_data("FE_H")
+scat = axes[2].scatter(x, y, c=c0, **kwds)
+
+if link:
+    axes[2].scatter(ux, uy, c=uc, alpha=0.5, marker="^", zorder=-1, **kwds)
+    for xi, uxi, uy, uyi in zip(x, ux, y, uy):
+        axes[2].plot([xi, uxi], [uy, uyi], c="#666666", zorder=-2, linewidth=0.5,
+            alpha=0.5)
+
+
+"""
+for comp in (bensby_2014_data, reddy_2003_data, reddy_2006_data, valenti_2005_data):
+
+    _, x = comp("TeffK")
+    _, y = comp("loggK")
+    _, c = comp("c_M_H_K")
+
+    axes[3].scatter(x, y, c=c, **kwds)
+"""
+
+for comp in (bensby_2014_data, reddy_2003_data, reddy_2006_data, valenti_2005_data):
+
+    _, x = comp("TEFF")
+    _, y = comp("LOGG")
+    _, c = comp("FE_H")
+
+    axes[-1].scatter(x, y, c=c, **kwds)
+
+
+xlim = np.array([ax.get_xlim() for ax in axes])
+ylim = np.array([ax.get_ylim() for ax in axes])
+for ax, label in zip(axes, comparison_labels):
+    #ax.set_xlim(xlim.max(), xlim.min())
+    #ax.set_ylim(ylim.max(), ylim.min())
+    
+    ax.set_xlim(7000, 4250)
+    ax.set_ylim(5, 2.5)
+    ax.xaxis.set_major_locator(MaxNLocator(6))
+    ax.yaxis.set_major_locator(MaxNLocator(6))
+
+    if ax.is_first_col():
+        ax.set_ylabel(r"$\log{g}$")
+
+    else:
+        ax.set_yticklabels([])
+
+    ax.set_xlabel(r"$T_{\rm eff}$ $[{\rm K}]$")
+
+    ax.set_title(label)
+
+
+
+cbar = plt.colorbar(scat, 
+    cax=fig.add_axes([0.93,fig.subplotpars.bottom,0.02,0.9 - fig.subplotpars.bottom]),
+    ticks=[-1.5, -1, -0.5, 0, 0.5])
+cbar.set_label(r"$[{\rm Fe/H}]$")
+
+fig.tight_layout()
+fig.subplots_adjust(right=0.92)
+
+
+
+fig.savefig("gold-standard-hrd.png")
+fig.savefig("gold-standard-hrd.pdf", dpi=300)
+
+
+
