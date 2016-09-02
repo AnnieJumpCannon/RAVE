3018be38dc4e110eb2f9439f9c749818be0371ac
Andy Casey <andycasey@gmail.com>
2016-09-02 14:34:22 +0100

    Add script for plotting Kordopatis calibration sample

---
+++ article/figures/plot_kordopatis_calibration_sample.py
@@ -0,0 +1,122 @@
+
+"""
+Plot giant abundances w.r.t. GES.
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import OrderedDict
+from matplotlib.ticker import MaxNLocator
+from matplotlib.colors import LogNorm
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+
+try:
+    rave_cannon_dr1, kordopatis_comparisons
+
+except NameError: # Do you know who I am? That's Jeff Vader!
+
+    from rave_io import get_cannon_dr1, get_kordopatis_comparisons
+
+    rave_cannon_dr1 = get_cannon_dr1()
+
+    kordopatis_comparisons = get_kordopatis_comparisons()
+    from astropy.table import join
+
+    data_table = join(rave_cannon_dr1, kordopatis_comparisons, keys=("Name", ))
+
+else:
+    print("Warning: Using pre-loaded data.")
+
+
+ok = (data_table["r_chi_sq"] < 3)# * (data_table["R"] > 10)
+
+latex_labels = {
+    "TEFF_2": r"$T_{\rm eff}$ $[{\rm K}]$ $({\rm Literature})$",
+    "TEFF_1": r"$T_{\rm eff}$ $[{\rm K}]$ $({\rm unRAVE})$",
+    "LOGG_1": r"$\log{g}$ $({\rm unRAVE})$",
+    "LOGG_2": r"$\log{g}$ $({\rm Literature})$",
+    "FE_H": r"$[{\rm Fe/H}]$ $({\rm unRAVE})$",
+    "FEH": r"$[{\rm Fe/H}]$ $({\rm Literature})$"
+}
+
+
+cannon_labels = ("TEFF_1", "LOGG_1", "FE_H")
+literature_labels = ("TEFF_2", "LOGG_2", "FEH")
+
+limits = {
+    "TEFF_1": [3500, 7000],
+    "LOGG_1": [0, 5],
+    "FE_H": [-4.5, 0.75]
+}
+
+kwds = dict(cmap="plasma", vmin=np.nanmin(data_table["snr"]), vmax=np.nanmax(data_table["snr"]))
+
+
+K = len(cannon_labels)
+factor = 3.5
+lbdim = 0.25 * factor
+tdim = 0.1 * factor
+rdim = 0.2 * factor
+whspace = 0.05
+yspace = factor
+xspace = factor * K + factor * (K - 1) * whspace + lbdim * (K - 1)
+xdim = lbdim + xspace + rdim
+ydim = lbdim + yspace + tdim
+
+fig, axes = plt.subplots(1, K, figsize=(xdim, ydim))
+fig.subplots_adjust(
+    left=lbdim/xdim, bottom=lbdim/ydim, right=(xspace + lbdim)/xdim,
+    top=(yspace + lbdim)/ydim, wspace=whspace, hspace=whspace)
+
+
+for i, (ax, cannon_label, literature_label) \
+in enumerate(zip(axes, cannon_labels, literature_labels)):
+
+    x = data_table[literature_label]
+    y = data_table[cannon_label]
+    c = data_table["snr"]
+
+    scat = ax.scatter(x[ok], y[ok], c=c[ok], s=50, alpha=0.5, **kwds)
+
+
+_ = ax.scatter([-999], [-9999], c=[0], **kwds)
+
+
+
+for ax, cannon_label, literature_label in zip(axes, cannon_labels, literature_labels):
+
+    lims = limits[cannon_label]
+    ax.plot(lims, lims, c="#666666", zorder=-1, linestyle=":")
+
+    ax.set_xlim(lims)
+    ax.set_ylim(lims)
+
+    ax.xaxis.set_major_locator(MaxNLocator(6))
+    ax.yaxis.set_major_locator(MaxNLocator(6))
+
+    ax.set_xlabel(latex_labels[literature_label])
+    ax.set_ylabel(latex_labels[cannon_label])
+
+
+fig.tight_layout()
+
+
+cbar = plt.colorbar(_, 
+    cax=fig.add_axes([0.93, fig.subplotpars.bottom, 0.02, fig.subplotpars.top - fig.subplotpars.bottom]))
+cbar.set_label(r"${\rm S/N}$ ${\rm unRAVE}$ $[{\rm pixel}^{-1}]$")
+
+
+fig.subplots_adjust(right=0.90)
+
+fig.savefig("kordopatis-calibration.pdf", dpi=300)
+fig.savefig("kordopatis-calibration.png")
+
+for ref in set(data_table["REF"]):
+    for cannon_label, literature_label in zip(cannon_labels, literature_labels):
+        match = data_table["REF"] == ref
+        x = data_table[cannon_label][match]
+        y = data_table[literature_label][match]
+        diff = y - x
+        print(ref, np.isfinite(diff).sum(), cannon_label, np.nanmean(diff), np.nanstd(diff))
