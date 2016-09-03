fec1d4c45ab68f0ceea9bbe51867c9c9014919fc
keithhawkins <astrohawk113@gmail.com>
2016-09-02 22:53:03 -0400

    Added GC isochrones, additions to plot_cluster.py

--- article/figures/plot_clusters.py
+++ article/figures/plot_clusters.py
@@ -10,15 +10,17 @@ from matplotlib.colors import LogNorm
 from matplotlib.ticker import MaxNLocator
 from astropy.table import join
 from astropy.table import Table
+import os
 
 
+rave_cannon_dr1 = Table.read('/Users/khawkins/Desktop/RAVE_cannon/unrave-v0.7-37_36.fits')
+RAVEDR4_GC = Table.read('/Users/khawkins/Desktop/RAVE_cannon/RAVEDR4_GC.fits')
+#RAVEDR4_GC = Table.read('/Users/khawkins/Desktop/RAVE_cannon/RAVEDR4_OC.fits')
 
-#rave_cannon_dr1 = Table.read('/Users/khawkins/Desktop/RAVE_cannon/rave-tgas-v1.fits')
-#RAVEDR4_GC = Table.read('/Users/khawkins/Desktop/RAVE_cannon/RAVEDR4_GC.fits')
-#data_table = join(rave_cannon_dr1, RAVEDR4_GC, keys=("Name",))
-#RAVEDR4 = Table.read('/Users/khawkins/Desktop/RAVE_cannon/RAVE-DR4.fits')
-#DR4_cannon = join(rave_cannon_dr1, RAVEDR4, keys=("Name",))
-
+data_table = join(rave_cannon_dr1, RAVEDR4_GC, keys=("Name",))
+RAVEDR4 = Table.read('/Users/khawkins/Desktop/RAVE_cannon/RAVE-DR4.fits')
+DR4_cannon = join(rave_cannon_dr1, RAVEDR4, keys=("Name",))
+vmax = -1.0 ; vmin=-3.0
 
 try:
     data_table
@@ -35,16 +37,21 @@ except NameError:
     data_table = join(get_cannon_dr1(), RAVEDR4_GC, keys=("Name",))
 
 else:
-    print("Warning: Using pre-loaded data!")
+    print "Warning: Using pre-loaded data!"
+
 
 DR4_cannon = data_table
 
+#----if Table has already c
 
 #---define unique clusters
-clusters = np.unique(data_table['NGC'])
+clusters = np.unique(data_table['Cluster'])
+
 #-------Generate HRD for each cluster----
 limits = [[3400,7000], [0.0,4.99]]
-K = len(clusters)
+K = len(clusters) 
+if K == 0:
+  raise ValueError('WARNING: No clusters found in the UNRAVE-tgas catalouge')
 factor = 3.5
 lbdim = 0.2 * factor
 trdim = 0.1 * factor
@@ -54,62 +61,80 @@ xspace = factor * K + factor * (K - 1) * whspace + lbdim * (K - 1)
 xdim = lbdim + xspace + trdim
 ydim = lbdim + yspace + trdim
 
-fig, axes = plt.subplots(K,2, figsize=(xdim, ydim), sharex=True,sharey=True)
+fig, axes = plt.subplots(2,K, figsize=(xdim, ydim), sharex=True,sharey=True)
 fig.subplots_adjust(left=lbdim/xdim, bottom=lbdim/ydim, 
   right=(xspace + lbdim)/xdim, top=(yspace + lbdim)/ydim, 
   wspace=whspace, hspace=whspace)
 
 
 for i in np.arange(K):
-  cind = np.where(data_table['NGC'] == clusters[i])[0] #find the cluster stars
+  cind = np.where(data_table['Cluster'] == clusters[i])[0] #find the cluster stars
   #plot the cannon
-  ax = axes[i][0]
-  vmin, vmax = -2, 0.5
-  scat = ax.scatter(data_table['TEFF'][cind], data_table['LOGG'][cind], c=data_table['FE_H'][cind], vmin=vmin, vmax=vmax)
+  ax = axes[0][i]
+  s = ax.scatter(data_table['TEFF'][cind], data_table['LOGG'][cind], c=data_table['FE_H'][cind],vmax=vmax,vmin=vmin,s=60 )
+  #plt.colorbar(s)
+  #---- plotting isochrones if they exist----
+  isopath = './' #defines the location of the isohrone data (Teff, logg info for the iso)
+  if os.path.isfile(isopath+'%s_y2_iso.dat'%clusters[i]):
+    T_iso, g_iso = np.loadtxt(isopath+'%s_y2_iso.dat'%clusters[i],unpack=True)
+    ax.plot(T_iso,g_iso,'k-')
+  #-----------------------------
+
   ax.set_xlim(limits[0])
   ax.set_ylim(limits[1])
   ax.xaxis.set_major_locator(MaxNLocator(6))
   ax.yaxis.set_major_locator(MaxNLocator(6))
   #ax.set_axis_bgcolor("#CCCCCC")
   ax.invert_xaxis();ax.invert_yaxis()
-  ax.text(6800,1.0,'NGC%i'%clusters[i])
-  ax.set_ylabel(r"$\log{g}$",)
+  ax.text(6800,1.0,'%s'%clusters[i])
+  #ax.set_ylabel(r"$\log{g}$",)
 
   if i==0:
-  	ax.set_title('The Cannon')
-  if i == K-1: 
-    ax.set_xlabel(r"$T_{\rm eff}$")
+    ax.set_ylabel(r"$\log{g}$",)
+  #	ax.set_title('The Cannon')
+  #if i == K-1: 
+  #  ax.set_xlabel(r"$T_{\rm eff}$")
 
-  ax = axes[i][1]
+  ax = axes[1][i]
   #plot the RAVE DR4
-  ax.scatter(data_table['TeffK'][cind], data_table['loggK'][cind], c=data_table['__M_H_K'][cind], vmin=vmin, vmax=vmax)
+  sc = ax.scatter(data_table['TeffK_1'][cind], data_table['loggK_1'][cind], c=data_table['__M_H_K_1'][cind], vmax=vmax,vmin=vmin,s=60)
+  #---- plotting isochrones if they exist----
+  isopath = './' #defines the location of the isohrone data (Teff, logg info for the iso)
+  if os.path.isfile(isopath+'%s_y2_iso.dat'%clusters[i]):
+    T_iso, g_iso = np.loadtxt(isopath+'%s_y2_iso.dat'%clusters[i],unpack=True)
+    ax.plot(T_iso,g_iso,'k-')
+  #-----------------------------
+  #plt.colorbar(sc)
   ax.set_xlim(limits[0])
   ax.set_ylim(limits[1])
   ax.xaxis.set_major_locator(MaxNLocator(6))
   ax.yaxis.set_major_locator(MaxNLocator(6))
   #ax.set_axis_bgcolor("#CCCCCC")
   ax.invert_xaxis();ax.invert_yaxis()
-  if i==0:
-  	ax.set_title('RAVE DR4')
+  ax.set_xlabel(r"$T_{\rm eff}$")
+  #if i==0:
+  #	ax.set_title('RAVE DR4')
   if i == K-1: 
-    ax.set_xlabel(r"$T_{\rm eff}$") 
+    #ax.set_xlabel(r"$T_{\rm eff}$") 
     ax.get_yaxis().set_visible(False)
+  if i==0:
+    ax.set_ylabel(r"$\log{g}$",)
 
-plt.colorbar(scat)
+plt.colorbar(sc)
 
 def generate_cluster_abundance_plot(cluster=clusters[0]):
   all_columns = [ 
   ("C_H", "N_H"), 
   ("C_H", "NA_H"),
   ]
-  cind = np.where(data_table['NGC'] == cluster)[0] #find the cluster stars
+  cind = np.where(data_table['Cluster'] == cluster)[0] #find the cluster stars
   RAlim=5
-  feild_ind = np.where((DR4_cannon['RAJ2000'] > data_table['RAJ2000'][cind[0]] - RAlim)&(DR4_cannon['RAJ2000'] < data_table['RAJ2000'][cind[0]] + RAlim) &\
-    (DR4_cannon['DEJ2000'] > data_table['DEJ2000'][cind[0]] - RAlim)&(DR4_cannon['DEJ2000'] < data_table['DEJ2000'][cind[0]] + RAlim))[0]
+  feild_ind = np.where((DR4_cannon['RAJ2000_1'] > data_table['RAJ2000_1'][cind[0]] - RAlim)&(DR4_cannon['RAJ2000_1'] < data_table['RAJ2000_1'][cind[0]] + RAlim) &\
+    (DR4_cannon['DEJ2000_1'] > data_table['DEJ2000_1'][cind[0]] - RAlim)&(DR4_cannon['DEJ2000_1'] < data_table['DEJ2000_1'][cind[0]] + RAlim))[0]
 
   plt.figure()
-  plt.plot(DR4_cannon['RAJ2000'][feild_ind],DR4_cannon['DEJ2000'][feild_ind],'.',color='gray')
-  plt.plot(data_table['RAJ2000'][cind],data_table['DEJ2000'][cind],'bo',ms=7)
+  plt.plot(DR4_cannon['RAJ2000_1'][feild_ind],DR4_cannon['DEJ2000_1'][feild_ind],'.',color='gray')
+  plt.plot(data_table['RAJ2000_1'][cind],data_table['DEJ2000_1'][cind],'bo',ms=7)
 
 
   fig, axes = plt.subplots(len(all_columns), 1)
@@ -119,7 +144,7 @@ def generate_cluster_abundance_plot(cluster=clusters[0]):
     xfield = DR4_cannon[columns[0]][feild_ind]
     yfield = DR4_cannon[columns[1]][feild_ind]
     ax.plot(xfield,yfield,'.',color='gray',label='Field',ms=2)
-    ax.plot(x,y,'bo', label='NGC%i'%cluster,ms=7)
+    ax.plot(x,y,'bo', label='%s'%cluster,ms=7)
     ax.set_ylim([-1,1]);ax.set_xlim([-1,1])
     #ax.legend()
     ax.set_xlabel('[%s/Fe]'%columns[0].split('_')[0])
@@ -131,22 +156,22 @@ def generate_cluster_abundance_plot(cluster=clusters[0]):
 
 def generate_cluster_memebership(cluster=clusters[0]):
   all_columns = [ 
-  ("HRV", "FE_H"), 
-  ("HRV", "__M_H_K"),
+  ("HRV_1", "FE_H"), 
+  ("HRV_1", "__M_H_K_1"),
   ]
-  cind = np.where(data_table['NGC'] == cluster)[0] #find the cluster stars
+  cind = np.where(data_table['Cluster'] == cluster)[0] #find the cluster stars
   RAlim=5
-  feild_ind = np.where((DR4_cannon['RAJ2000'] > data_table['RAJ2000'][cind[0]] - RAlim)&(DR4_cannon['RAJ2000'] < data_table['RAJ2000'][cind[0]] + RAlim) &\
-    (DR4_cannon['DEJ2000'] > data_table['DEJ2000'][cind[0]] - RAlim)&(DR4_cannon['DEJ2000'] < data_table['DEJ2000'][cind[0]] + RAlim))[0]
+  feild_ind = np.where((DR4_cannon['RAJ2000_1'] > data_table['RAJ2000_1'][cind[0]] - RAlim)&(DR4_cannon['RAJ2000_1'] < data_table['RAJ2000_1'][cind[0]] + RAlim) &\
+    (DR4_cannon['DEJ2000_1'] > data_table['DEJ2000_1'][cind[0]] - RAlim)&(DR4_cannon['DEJ2000_1'] < data_table['DEJ2000_1'][cind[0]] + RAlim))[0]
 
-  x = data_table['HRV'][cind]
+  x = data_table['HRV_1'][cind]
   y = data_table['FE_H'][cind]
-  xfield = DR4_cannon['HRV'][feild_ind]
+  xfield = DR4_cannon['HRV_1'][feild_ind]
   yfield = DR4_cannon['FE_H'][feild_ind]
   plt.figure(figsize=(xdim, ydim))
   ax1 = plt.subplot(1,2,1)
   ax1.plot(xfield,yfield,'.',color='gray',label='Field',ms=2)
-  ax1.plot(x,y,'bo',label='NGC%i'%cluster,ms=7)
+  ax1.plot(x,y,'bo',label='%s'%cluster,ms=7)
   ax1.set_title('The Cannon')
   ax1.set_ylabel('[Fe/H]')
   ax1.set_xlabel('HRV (km/s)')
@@ -156,24 +181,26 @@ def generate_cluster_memebership(cluster=clusters[0]):
 
 
   ax2 = plt.subplot(1,2,2,sharex=ax1,sharey=ax1)
-  y = data_table['__M_H_K'][cind]
-  xfield = DR4_cannon['HRV'][feild_ind]
-  yfield = DR4_cannon['__M_H_K'][feild_ind]
+  y = data_table['__M_H_K_1'][cind]
+  xfield = DR4_cannon['HRV_1'][feild_ind]
+  yfield = DR4_cannon['__M_H_K_1'][feild_ind]
   ax2.plot(xfield,yfield,'.',color='gray',label='Field',ms=2)
-  ax2.plot(x,y,'bo',label='NGC%i'%cluster,ms=7)
+  ax2.plot(x,y,'bo',label='%s'%cluster,ms=7)
   ax2.set_title('RAVE DR4')
   ax2.set_ylabel('[Fe/H]')
   ax2.set_xlabel('HRV (km/s)')
   ax2.set_ylim([-2.0,0.6]) ; ax2.set_xlim([-400,400])     
   ax2.get_yaxis().set_visible(False)
 
-  plt.tight_layout()
+  #plt.tight_layout()
 
 
 
 for i in np.arange(len(clusters)):
   #generate_cluster_abundance_plot(cluster=clusters[i])
   generate_cluster_memebership(cluster=clusters[i])
+
+
 
 
 
